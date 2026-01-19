30fc8f41eaa7ecc7fe06c86fcd43196d4ed77ca2
Carol Reis <reicaroline@gmail.com>
2026-01-19 15:41:12 -0300

    atualiza pasta de exercicios

--- mundo_1/tratando-dados-fazendo-contas/ex004.py
+++ mundo_1/tratando-dados-fazendo-contas/ex004.py
@@ -4,10 +4,11 @@
 
 info_usuario = input('Digite algo: ')
 
-print(f'Tipo primitivo: {type(info_usuario)}')
+print(f'O tipo primitivo é: {type(info_usuario)}')
 print(f'É numérico? {info_usuario.isnumeric()}')
 print(f'É alfabético? {info_usuario.isalpha()}')
 print(f'É alfanumerico? {info_usuario.isalnum()}')
 print(f'É minuscula? {info_usuario.islower()}')
 print(f'É maiuscula? {info_usuario.isupper()}')
 print(f'Contém espaço? {info_usuario.isspace()}')
+print(f'Está capitalizada? {info_usuario.istitle()}')
