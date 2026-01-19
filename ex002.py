8974f7677e77a6b01c4fb1d727951cd22f37d9d8
Carol Reis <reicaroline@gmail.com>
2026-01-19 15:08:36 -0300

    atualizando pastas

--- ex002.py
+++
@@ -1,3 +0,0 @@
-nome_usuario = input('Digite seu nome: ')
-
-print(f'Prazer em te conhecer, {nome_usuario}!')
---
+++ mundo_1/primeiros_passos/ex002.py
@@ -0,0 +1,7 @@
+"""
+Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado
+"""
+
+nome_usuario = input('Digite seu nome: ')
+
+print(f'Prazer em te conhecer, {nome_usuario}!')
